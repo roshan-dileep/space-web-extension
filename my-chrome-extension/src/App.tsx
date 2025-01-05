ccd28a0c283911a5f9c959640e376d3b76c27f5d
RoshanDileep <roshandileep007@gmail.com>
2025-01-04 22:05:22 -0800

    Complete rewrite
    Instead of webpack use vite

---
+++ my-chrome-extension/src/App.tsx
@@ -0,0 +1,7 @@
+import Popup from "./Components/Popup";
+
+const App: React.FC = () => {
+  return <Popup />;
+};
+
+export default App;
---
+++ my-chrome-extension/src/Components/Popup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const Popup: React.FC = () => (
+  <div className="p-4">
+	  <h1 className="text-lg font-bold">
+		Hello, Chrome Extension!
+	  </h1> 
+  </div> 
+);
+
+export default Popup;
