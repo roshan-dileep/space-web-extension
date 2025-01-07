385603f8d8c6cff947a213becc6307c04a1ba4f5
RoshanDileep <roshandileep007@gmail.com>
2025-01-06 18:37:21 -0800

    Add basic input

--- my-chrome-extension/src/Components/Panel.tsx
+++ my-chrome-extension/src/Components/Panel.tsx
@@ -8,7 +8,18 @@ const Panel = () => {
         boxShadow: '0px 0px 5px #0000009e', // Optional shadow
       }}
     >
-      {/* Blank side panel */}
+      <input style={{
+        
+        position: 'absolute',
+        // make it a little higher than the bottom
+
+        bottom: '50px',
+        width: '100%',
+        padding: '5px',
+        border: '10px solid #0000009e',
+        backgroundColor: 'white',
+
+      }}/>  
     </div>
   );
 };
