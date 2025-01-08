d89134b3585f05d8d16650315573c9e407520a31
RoshanDileep <roshandileep007@gmail.com>
2025-01-08 10:30:32 -0800

    Update extension and stuff

--- my-chrome-extension/src/Components/Panel.tsx
+++ my-chrome-extension/src/Components/Panel.tsx
@@ -1,13 +1,16 @@
+import  { useState } from 'react';
+
 const Panel = () => {
+  const [query, setQuery] = useState('');
 
   function sendQueryData() {
-    fetch('http://localhost:5000/api/query', {
+    fetch('http://localhost:8080/query', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        query: 'SELECT * FROM users',
+        query: query,
       }),
     })
       .then((response) => response.json())
@@ -17,45 +20,52 @@ const Panel = () => {
       .catch((error) => {
         console.error('Error:', error);
       });
-
   }
-  sendQueryData();
+
   return (
     <div
       style={{
-        width: '300px', // Adjust the width as needed
-        height: '100%', // Full height
-        backgroundColor: 'white', // Background color
-        boxShadow: '0px 0px 5px #0000009e', // Optional shadow
+        width: '300px', 
+        height: '100%', 
+        backgroundColor: 'black', 
+        boxShadow: '0px 0px 5px #0000009e', 
+        position: 'relative',
       }}
     >
-      <input style={{
-        
-        position: 'absolute',
-        
-
-        bottom: '50px',
-        width: '100%',
-        padding: '5px',
-        border: '10px solid #0000009e',
-        backgroundColor: 'white',
-
-      }}/>  
+      <input 
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        style={{
+          position: 'absolute',
+          bottom: '50px',
+          width: '100%',
+          padding: '5px',
+          border: '1px solid #0000009e',
+          backgroundColor: 'black',
+          color: 'white',
+        }}
+      />  
 
       <button 
-      
-      style={{
-        position: 'absolute',
-        
-        bottom: '200px',
-        
-        
-        width: '100%',
-        padding: '5px',
-        border: '10px solid #0000009e',
-        backgroundColor: 'white',
-      }}
-      />
+        onClick={sendQueryData}
+        style={{
+          position: 'absolute',
+          bottom: '10px',
+          right: '10px',
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%',
+          border: 'none',
+          backgroundColor: '#007bff',
+          color: 'white',
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Go
+      </button>
     </div>
   );
 };
