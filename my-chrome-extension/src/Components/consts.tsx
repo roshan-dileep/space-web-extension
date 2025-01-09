4db0236a7cc5b1caadcd335ea107355912d87fd9
RoshanDileep <roshandileep007@gmail.com>
2025-01-08 21:20:42 -0800

    Get rid of stuff

--- my-chrome-extension/src/Components/consts.tsx
+++
@@ -1,8 +0,0 @@
-export const APP_COLLAPSE_WIDTH = 50; // Width when collapsed
-export const APP_EXTEND_WIDTH = 300; // Width when expanded
-
-export const URLS = [
-  { name: 'Google', url: 'https://www.google.com', image: 'https://via.placeholder.com/50' },
-  { name: 'YouTube', url: 'https://www.youtube.com', image: 'https://via.placeholder.com/50' },
-  { name: 'Facebook', url: 'https://www.facebook.com', image: 'https://via.placeholder.com/50' },
-];
