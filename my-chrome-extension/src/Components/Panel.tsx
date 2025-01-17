29bd772390c3b87f1bcfb94125524073359d4695
RoshanDileep <roshandileep007@gmail.com>
2025-01-16 17:37:59 -0800

    fiexed

--- my-chrome-extension/src/Components/Panel.tsx
+++ my-chrome-extension/src/Components/Panel.tsx
@@ -1,72 +1,10 @@
-import  { useState } from 'react';
+
+import './Panel.css'; // Import the CSS file for the Panel component
 
 const Panel = () => {
-  const [query, setQuery] = useState('');
-
-  function sendQueryData() {
-    fetch('http://localhost:8080/query', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
-
   return (
-    <div
-      style={{
-        width: '300px', 
-        height: '100%', 
-        backgroundColor: 'black', 
-        boxShadow: '0px 0px 5px #0000009e', 
-        position: 'relative',
-      }}
-    >
-      <input 
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        style={{
-          position: 'absolute',
-          bottom: '50px',
-          width: '100%',
-          padding: '5px',
-          border: '1px solid #0000009e',
-          backgroundColor: 'black',
-          color: 'white',
-        }}
-      />  
-
-      <button 
-        onClick={sendQueryData}
-        style={{
-          position: 'absolute',
-          bottom: '10px',
-          right: '10px',
-          width: '100px',
-          height: '100px',
-          borderRadius: '50%',
-          border: 'none',
-          backgroundColor: 'purple',
-          color: 'white',
-          fontSize: '20px',
-          cursor: 'pointer',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        Go
-      </button>
+    <div className="panel">
+      {/* Purple screen */}
     </div>
   );
 };
--- my-chrome-extension/src/main.tsx
+++ my-chrome-extension/src/main.tsx
@@ -1,6 +1,7 @@
 
 import ReactDOM from 'react-dom';
 import Panel from './Components/Panel';
+import './index.css'; // Ensure the global CSS is imported
 
 const App = () => {
   return (
