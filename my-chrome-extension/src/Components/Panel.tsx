24f2f0d5b91bfa06bb259d4c2870691c4500e2fe
RoshanDileep <roshandileep007@gmail.com>
2025-01-17 17:45:33 -0800

    fixed up

--- my-chrome-extension/src/Components/Panel.tsx
+++ my-chrome-extension/src/Components/Panel.tsx
@@ -2,29 +2,35 @@ import { useState } from 'react';
 import '../index.css'; // Importing global CSS
 import './Panel.css'; // Importing Panel-specific CSS
 import { Input } from './ui/input'; // Adjusting the import path
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
 
 const Panel = () => {
   const [query, setQuery] = useState('');
   const [response, setResponse] = useState(null);
+  const [sources, setSources] = useState(null);
 
   const fetchData = async () => {
     try {
       const response = await fetch(`http://127.0.0.1:8080/query?query=${encodeURIComponent(query)}`);
       const result = await response.json();
+      
       setResponse(result.response); // Set the response field
+      setSources(result.sources);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
   return (
-    <div className="bg-black w-screen h-screen flex flex-col items-center justify-end pb-10">
-      {response && (
-        <div className="result mt-4 text-white w-full px-4">
-          <pre className="whitespace-pre-wrap break-words">{response}</pre>
-        </div>
-      )}
-      <div className="flex items-center w-full justify-center mb-4">
+    <div className="bg-black w-screen h-screen flex flex-col items-center justify-end pb-20">
+      <div className="flex items-center w-full justify-center mb-20">
         <Input
           className="input w-7/10 md:w-96"
           type="text"
@@ -36,6 +42,19 @@ const Panel = () => {
           Search
         </button>
       </div>
+      {response && (
+        <div className="result mt-4 text-white w-full px-4 mb-10">
+          <pre className="whitespace-pre-wrap break-words">{response}</pre>
+          <DropdownMenu>
+            <DropdownMenuTrigger>Sources</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>0.9</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>{sources}</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
     </div>
   );
 };
